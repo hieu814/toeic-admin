54593011d5d8b81216ae1e30f1b84cf9ff40914a
hieu <hieuvu81198@gmail.com>
2023-06-01 02:16:15 +0700

    add random question

--- src/views/admin/Exam/components/ExamModal.js
+++ src/views/admin/Exam/components/ExamModal.js
@@ -6,8 +6,26 @@ import { useAddExamMutation, useUpdateExamMutation } from 'src/api/exam';
 import { removeUndefined } from 'src/common/Funtion';
 import CustomUpload from 'src/components/CustomUpload';
 import { UploadOutlined } from '@ant-design/icons';
+import { useBulkInsertQuestionsMutation, useFindAllQuestionsMutation } from 'src/api/question';
 const { Option } = Select;
-
+const fullTestQuestions = {
+    1: 6,
+    2: 25,
+    3: 13,
+    4: 10,
+    5: 30,
+    6: 100,
+    7: 200,
+}
+const miniTestQuestions = {
+    1: 2,
+    2: 7,
+    3: 3,
+    4: 3,
+    5: 8,
+    6: 2,
+    7: 2,
+}
 const formItemLayout = {
     labelCol: {
         xs: {
@@ -31,16 +49,30 @@ const ExamModal = (props) => {
     const [form] = Form.useForm();
     const [addExam, { error: insertError, isLoading: insertLoading = false }] = useAddExamMutation();
     const [updateExam, { error: updateError, isLoading: updateLoading = false }] = useUpdateExamMutation();
+    const [findAllQuestions] = useFindAllQuestionsMutation();
+    const [bulkInsertQuestion] = useBulkInsertQuestionsMutation();
     const onFinish = async (values) => {
         values = removeUndefined(values)
-        console.log(values);
+
+        // return
         if (isInsert) {
             addExam(values)
-                .then((respond) => {
+                .then(async (respond) => {
 
-                    console.log(respond);
-                    message.success(respond.message)
-                    // handleCalcel()
+                    if (respond.data.data.id && values.random) {
+                        await updateQuestions(respond.data.data.id, values.type).then(async (respond) => {
+                            message.success(respond.message)
+                            handleCalcel()
+                        })
+                            .catch((err) => {
+                                console.log(err);
+
+                            });
+                    } else {
+                        message.success(respond.message)
+                        handleCalcel()
+                    }
+
                 })
                 .catch((err) => {
                     console.log(err);
@@ -50,18 +82,139 @@ const ExamModal = (props) => {
             values.id = data?.id
             updateExam(values)
                 .unwrap()
-                .then((respond) => {
-                    message.success(respond.message)
-                    handleCalcel()
+                .then(async (respond) => {
+                    if (respond.data.data.id && values.random) {
+                        await updateQuestions(respond.data.data.id, values.type).then(async (respond) => {
+                            message.success(respond.message)
+                            handleCalcel()
+                        })
+                            .catch((err) => {
+                                console.log(err);
+
+                            });
+                    } else {
+                        message.success(respond.message)
+                        handleCalcel()
+                    }
                 })
                 .catch((err) => {
                     console.log(err);
 
                 });
         }
-        handleCalcel()
+        // handleCalcel()
 
     };
+    async function updateQuestions(examId, type) {
+        return new Promise(async (resolve, reject) => {
+            const questions = await getRandomQuestion(type)
+            console.log(questions);
+            bulkInsertQuestion({
+                data: questions
+            })
+                .unwrap()
+                .then((respond) => {
+                    message.success(respond.message)
+                    // console.log(respond?.data || []);
+                    updateExam({
+                        id: examId,
+                        $addToSet: { questions: respond?.data || [] }
+                    })
+                        .unwrap()
+                        .then((respond) => {
+                            resolve(respond)
+                        })
+                        .catch((err) => {
+                            reject(err)
+                        });
+                })
+                .catch((err) => {
+                    reject(err)
+
+                });
+        })
+    }
+    function getRandomQuestion(examType) {
+        const types = examType < 2 ? [1, 2, 3, 4, 5, 6, 7] : [examType - 1];
+        const promises = [];
+
+        var questionsCntMap = examType === 1 ? miniTestQuestions : fullTestQuestions;
+
+        types.forEach(type => {
+
+            const promise = findAllQuestions({
+                random: true,
+                size: questionsCntMap[type] ?? 0,
+                query: [
+                    {
+                        $match: {
+                            type: type
+                        }
+                    },
+                    { $sample: { size: questionsCntMap[type] ?? 0, } }
+                ]
+            });
+            promises.push(promise);
+        });
+        return Promise.all(promises)
+            .then(responses => {
+                if (!responses || !Array.isArray(responses)) {
+                    return []
+                }
+                var allQuestions = [];
+                for (let i = 0; i < responses.length; i++) {
+                    var array = responses[i].data.data ?? []
+                    if (examType === 0 && array.length > 0 && (array[0].type === 6 || array[0].type === 7)) {
+                        array = getRandomReading(array, array[0].type === 7 ? 54 : 16)
+                        console.log({ reading: array });
+                    }
+                    allQuestions = allQuestions.concat(array);
+                }
+                return allQuestions;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
+            .then(allQuestions => {
+                var documents = allQuestions.sort((a, b) => a.type - b.type);
+                // Remove specified keys from each document
+                const modifiedDocuments = documents.map(({ _id, addedBy, createdAt, updatedAt, isDeleted, isActive, __v, updatedBy, ...rest }) => rest);
+                var currentQuestionNum = 1
+                const convertedArray = modifiedDocuments.map(obj => {
+                    const _questions = obj.questions.map(question => {
+                        return { ...question, number: currentQuestionNum++ };
+                    });
+                    return { ...obj, questions: _questions, group: _questions.length < 2 ? `${_questions[0].number}` : `${_questions[0].number}-${_questions[_questions.length - 1].number}` };
+                });
+                return convertedArray
+
+            });
+    }
+    function getRandomReading(records, targetSum) {
+        let sum = 0;
+        let sequence = [];
+
+        for (let i = 0; i < records.length; i++) {
+            const record = records[i];
+            sum += record.questions.length;
+            sequence.push(record);
+
+            if (sum === targetSum) {
+                return sequence;
+            } else if (sum > targetSum) {
+                while (sum > targetSum) {
+                    const removedRecord = sequence.shift();
+                    sum -= removedRecord.questions.length;
+                }
+
+                if (sum === targetSum) {
+                    return sequence;
+                }
+            }
+        }
+
+        return [];
+    }
     function handleCalcel() {
         form.resetFields();
         onCancel()
@@ -170,7 +323,7 @@ const ExamModal = (props) => {
                         <CustomUpload />
                     </Form.Item> */}
 
-                    <Form.Item name="isActive" label="Active" valuePropName="checked">
+                    <Form.Item name="random" label="Random question" valuePropName="checked">
                         <Switch />
                     </Form.Item>
                 </Form>
